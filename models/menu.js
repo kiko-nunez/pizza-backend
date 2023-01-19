ebe0ced0c3b7d119551a865ee24c2745b6ea3338
krsnamara <martinj.fitzpatrick@gmail.com>
2023-01-18 20:47:20 -0500

    updating menu to reflect stream line launch build

--- models/menu.js
+++ models/menu.js
@@ -2,23 +2,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Menu Schema
+// User Schema
 const menuSchema = Schema({
-  
   item: String,
   description: String,
-  img: String,
   price: String,
-
-  shop: {
-		type: Schema.Types.ObjectId,
-		ref: 'Shop',
-	},
+  // shop: {
+	// 	type: Schema.Types.ObjectId,
+	// 	ref: 'Shop',
+	//   },
   },  {
     timestamps: true
   });
 
-// Building Menu Model In MongoDB
+// User Model
 const Menu = mongoose.model('Menu', menuSchema);
 
 // Export User Model
