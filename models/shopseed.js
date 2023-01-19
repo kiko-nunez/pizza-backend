59f9dcbaaaec8012a3811627687e8452abfb7358
krsnamara <martinj.fitzpatrick@gmail.com>
2023-01-18 23:08:57 -0500

    adding shop seed model

---
+++ models/shopseed.js
@@ -0,0 +1,57 @@
+// Seed data
+module.exports = [
+    {
+        name: 'Joes Pizza',
+        adress: '216 Bedford Ave, Brooklyn, NY 11249',
+        phone: '718-736-2676',
+        
+    },
+    {
+        name: 'San Remo Pizzeria & Restaurant',
+        adress: '1408 Cortelyou Rd, Brooklyn, NY 11226',
+        phone: '347-754-3941',
+        
+    },
+    {
+        name: 'Rosas Pizza',
+        adress: '374 Metropolitan Ave, Brooklyn, NY 11211',
+        phone: '718-963-2378',
+        
+    },
+    {
+        name: 'Artichoke Basilles Pizza',
+        adress: '321 E 14th St, New York, NY 10003',
+        phone: '212-228-2004',
+        
+    },
+    {
+        name: 'Anna Maria Pizza & Pasta',
+        adress: '179 Bedford Ave, Brooklyn, NY 11211',
+        phone: '718-599-4550',
+        
+    },
+    {
+        name: 'Stromboli Pizza',
+        adress: '83 St Marks Pl, New York, NY 10003',
+        phone: '212-673-3691',
+        
+    },
+    {
+        name: 'Brooklyn Pizza Crew',
+        adress: '758 Nostrand Ave., Brooklyn, NY 11216',
+        phone: '718-363-1122',
+        
+    },
+    {
+        name: 'Saraghina Pizzeria',
+        adress: '350 Lewis Ave, Brooklyn, NY 11233',
+        phone: '718-574-0010',
+        
+    },
+    {
+        name: 'Di Fara Pizza',
+        adress: '1424 Avenue J, Brooklyn, NY 11230',
+        phone: '718-258-1367',
+        
+    },
+]
