80ebef5f005e39c39a9c121d2545b77bc37815de
krsnamara <martinj.fitzpatrick@gmail.com>
2023-01-18 23:43:42 -0500

    refactored for user vs customer

--- controllers/user.js
+++ controllers/user.js
@@ -1,64 +1,54 @@
 // Dependencies
 const express = require('express');
 const userRouter = express.Router();
-const Customer = require('../models/customer.js');
+const User = require('../models/user.js');
 
 // Routes //
-
-// Seed
-const seed = require('../models/shopseed');
-userRouter.get('/seed', (req, res) => {
-    Customer.deleteMany({}, (error, allCustomers) => {});
-
-    Customer.create(seed, (error, data) => {
-        res.redirect('/');
-    });
-});
 
 // create a test route //
 userRouter.get("/", (req, res) => {
     res.send("hello world");
 });
 
-// Customer INDEX ROUTE
-userRouter.get("/customer", async (req, res) => {
+// User INDEX ROUTE
+userRouter.get("/user", async (req, res) => {
     try {
         // send all people
-        res.json(await Customer.find({}));
+        res.json(await User.find({}));
     } catch (error) {
         // send error
         res.status(400).json(error);
     }
 });
 
-// Customer CREATE ROUTE
-userRouter.post("/customer", async (req, res) => {
+// User CREATE ROUTE
+userRouter.post("/user", async (req, res) => {
     try {
         // send all people
-        res.json(await Customer.create(req.body));
+        res.json(await User.create(req.body));
     } catch (error) {
         //send error
         res.status(400).json(error);
     }
 });
 
-// Customer DELETE ROUTE
-userRouter.delete("/customer/:id", async (req, res) => {
+// User DELETE ROUTE
+userRouter.delete("/user/:id", async (req, res) => {
     try {
         // send all people
-        res.json(await Customer.findByIdAndRemove(req.params.id));
+        res.json(await User.findByIdAndRemove(req.params.id));
     } catch (error) {
         // send error
         res.status(400).json(error);
     }
 });
 
-// Customer UPDATE ROUTE
-userRouter.put("/customer/:id", async (req, res) => {
+// User UPDATE ROUTE
+userRouter.put("/user/:id", async (req, res) => {
     try {
         //send all people
         res.json(
-            await Customer.findByIdAndUpdate(req.params.id, req.body, { new: true})
+            await User.findByIdAndUpdate(req.params.id, req.body, { new: true})
         );
     } catch (error) {
         // send error
