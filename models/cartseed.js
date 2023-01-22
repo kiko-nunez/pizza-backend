6ac7789208979efa3f9193ac55f4160cc7f477dc
krsnamara <martinj.fitzpatrick@gmail.com>
2023-01-22 00:41:28 -0500

    cart seed updated

---
+++ models/cartseed.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        item: 'Cheese',
+        image: 'https://slice-menu-assets-prod.imgix.net/24773/1593710713_4e14254a51?fit=crop&w=320&h=320',
+        description: 'Classic cheese or create your own pizza.',
+        price: '$15.00',
+    },
+    {
+        item: 'Grandma Cheese Pizza',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Grandma-pizza-01.jpg/440px-Grandma-pizza-01.jpg',
+        description: 'Fresh mozzarella, regular mozzarella, basil, marinara.',
+        price: '$26.00',
+    },
+    {
+        item: 'Sicilian Pie',
+        image: 'https://www.sipandfeast.com/wp-content/uploads/2021/08/new-york-sicilian-recipe-snippet.jpg',
+        description: 'San Marzano sauce, and mozzarella.',
+        price: '$22.00',
+    },
+    {
+        item: 'Caesar Salad',
+        image: 'https://www.jocooks.com/wp-content/uploads/2020/07/caesar-salad-1-12.jpg',
+        description: 'Crisp romaine tossed with croutons, Caesar dressing, and grated cheese.',
+        price: '$8.50',
+    },
+    {
+        item: 'Soft Drink',
+        image: 'https://img-aws.ehowcdn.com/700x/www.onlyinyourstate.com/wp-content/uploads/2020/06/2700695302_91ddd176b3_k.jpg',
+        description: 'Pick from an spectacular selection of Pop.',
+        price: '$1.50',
+    },
+]
